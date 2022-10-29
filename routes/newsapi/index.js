f7f9950e8fd81f7e997c5ba8d6d5369973bbdad2
Dylan Moore <alloydylan@icloud.com>
2022-10-29 04:47:31 -0400

    added dating

--- routes/newsapi/index.js
+++ routes/newsapi/index.js
@@ -1,14 +1,13 @@
-const { createSecretKey } = require("crypto");
-const { response } = require("express");
 const express = require("express");
 const router = express.Router();
+const moment = require("moment");
 
 router.post('/everything', function (req, res, next) {
     const https = require("https");
     const options = {
         headers: {"User-Agent": "Digits3 News API"}
     }
-    https.request(`https://newsapi.org/v2/everything?q=${req.body.query}&from=2022-09-28&sortBy=publishedAt&apiKey=912f1d27a98c4a75a4d26c7cb2de2157&language=en`, options, (response) => {
+    https.request(`https://newsapi.org/v2/everything?q=${req.body.query}&from=${moment().format().split("T")[0]}&sortBy=publishedAt&apiKey=912f1d27a98c4a75a4d26c7cb2de2157&language=en`, options, (response) => {
         let data = '';
         response.on('data', (chunk) => {
             data = data + chunk.toString();
