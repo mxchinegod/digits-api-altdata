ae43742c376b0fc73640847f70f93154cdc55336
Dylan <alloydylan@icloud.com>
2023-02-07 14:12:37 -0500

    💥 feat(wip): add API to Federal Reserve site

---
+++ routes/reserve/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+const moment = require("moment");
+
+/* This is a get request to the Federal Reserve website. */
+router.get('/schedule', function (req, res, next) {
+    const https = require("https");
+    const options = {
+        headers: {"User-Agent": "Digits3 News API"}
+    }
+    https.request(`https://www.federalreserve.gov/json/calendar.json`, options, (response) => {
+        let data = '';
+        response.on('data', (chunk) => {
+            data = data + chunk.toString();
+        })
+        response.on('end', () => {
+            res.json(JSON.parse(data))
+        })
+    })
+    .on('error', (error) => {
+        console.log('An error', error);
+    })
+    .end()
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -6,8 +6,8 @@ app.use(cors())
 app.use(express.json())
 app.use("/",                require("./routes/index"))
 app.use("/darkpool",        require("./routes/darkpool/index"))
-app.use("/news",        require("./routes/newsapi/index"))
-
+app.use("/news",            require("./routes/newsapi/index"))
+app.use("/reserve"),        require("./routes/reserve/index")
 /* Listening to the port that is defined in the config file. */
 app.listen(config.servicePort,function(){
     console.log(`Now listening on ${config.servicePort}`)
