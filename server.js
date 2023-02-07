310cc645bfa3366ad797f3ea5142b0138355b1fd
Dylan <alloydylan@icloud.com>
2023-02-07 14:21:46 -0500

    🐛 fix(typos): bad parentheses

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.json())
 app.use("/",                require("./routes/index"))
 app.use("/darkpool",        require("./routes/darkpool/index"))
 app.use("/news",            require("./routes/newsapi/index"))
-app.use("/reserve"),        require("./routes/reserve/index")
+app.use("/reserve",        require("./routes/reserve/index"))
 /* Listening to the port that is defined in the config file. */
 app.listen(config.servicePort,function(){
     console.log(`Now listening on ${config.servicePort}`)
